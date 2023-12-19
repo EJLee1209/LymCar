cf5387e982d9f74a589a144192e27188e14a65a1
EJLee1209 <dldmswo1209@gmail.com>
2023-12-19 15:06:41 +0900

    Initial Commit

---
+++ LymCar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LymCar
+//
+//  Created by 이은재 on 12/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LymCar/LymCarApp.swift
@@ -0,0 +1,17 @@
+//
+//  LymCarApp.swift
+//  LymCar
+//
+//  Created by 이은재 on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct LymCarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
